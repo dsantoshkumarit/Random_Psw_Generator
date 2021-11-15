5eff167797260ff4da8248bebf3a5009f3acb70e
Santosh Divate <dsantoshkumarit@gmail.com>
2021-11-15 10:16:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const info = document.querySelector(".info");
+const passField = document.querySelector("#password");
+let password = "";
+
+function generatePassword() {
+  password = "";
+  let length = 8;
+  let chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+  let n = chars.length;
+  for (var i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * n));
+  }
+  passField.value = password;
+}
+
+function copyText() {
+  navigator.clipboard.writeText(password);
+  info.style.opacity = "1";
+  setTimeout(() => (info.style.opacity = "0"), 1500);
+}
